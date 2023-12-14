71f198970a33bf0523ce9e61541b8418164f08a4
Yhomarth Reyes <yhomarth@gmail.com>
2023-12-14 14:07:07 -0400

    Cron task

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,11 +1,22 @@
 
-
+import { CronJob } from 'cron';
 
 export class ServerApp {
 
 
     static start() {
         console.log('Server started..');
+
+
+        const job = new CronJob(
+            '*/3 * * * * *', // cronTime
+            function () {
+                console.log('You will see this message every three second');
+            }
+        );
+        job.start();
+
+
     }
 
 }
